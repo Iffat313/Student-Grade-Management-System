e126a4b2543233f96d216edf8420d75552c3aef5
Iffat Rahman <iffatrahman99@gmail.com>
2025-12-01 16:51:21 -0500

    Configured the GPA methods for the class admin in for displaying said stats from the sudent data file.

--- SGMS.cpp
+++ SGMS.cpp
@@ -15,9 +15,12 @@ string LogIn();
 
 class Admin{
     public: //access specifier
-        float ClassAverageGPA(); //returns the class average GPA via int
-        float LowestGPA(); //returns the lowest GPA in the class via float
-        float HighestGPA(); //returns the highest GPA in the class via float
+        float ClassAverageGPA(); //returns the class average GPA via float. So we must consider every student.
+        void LowestGPA(); //returns the lowest GPA in the class via float. Calculate the GPA for every student and return the GPA of student who has the Lowest
+        void HighestGPA(); //returns the highest GPA in the class via float. Calculate the GPA for every student 
+        /*
+        - GPA = Sum of the grade(s)/number of grades
+        */
         void GradeDistribution(); //displays students who got A, B, etc
         void SortByName();
         void SortByGrade();
@@ -72,6 +75,11 @@ int main(){
     do{ //for admin
         cout << "1. Logout \n";
         cout << "2. Add a Student Record \n";
+        cout << "3. Retrieve The Class Average \n";
+        cout << "4. Retrieve the highest GPA in the class \n";
+        cout << "5. Retrieve the lowest GPA in the class \n";
+        cout << "6. Retrieve Student Record \n";
+        cout << "7. Retrieve Grade Distribution \n";
         cout << "Enter: ";
         cin >> UserInput;
 
@@ -82,13 +90,30 @@ int main(){
         else if(UserInput == 2){
             AdminObject.AddStudentRecord();
         }
+
+        else if(UserInput == 3){
+            cout << "The class average is: " << AdminObject.ClassAverageGPA() << "\n";
+        }
+
+        else if(UserInput == 4){
+            AdminObject.HighestGPA();
+        }
+
+        else if(UserInput == 5){
+            AdminObject.LowestGPA();
+        }
+
+        else if(UserInput == 6){
+            AdminObject.LoadStudentRecord();
+        }
+
+        else if(UserInput == 7){
+            AdminObject.GradeDistribution();
+        }
         
     }while((AccessGranted!='N') && (UserType == "Admin"));
 
 }
-
-
-
 
 string LogIn(){
     //local variable use and everytime this method everything happens again cincluding re-declaring the obkect from ifstream
@@ -96,15 +121,15 @@ string LogIn(){
     vector<string>Words;
 
 
-    ifstream File("UserCreds.txt"); // read from the text file , ifstream is an object from ftream library to read from the text file
+    ifstream File("UserCreds.txt"); // read from the text file , ifstream is a class from the fstream library to read from the text file. "File" is the object/instance of the class ifstream, which calls the pamatrized construcotr to open and read from a file
 
     while(getline(File, Text)){ //as we traverse through the while loop, Text will read/intialized by every char from the file until it see's a new line, thus reading the file line by line in each iteration. Thus, 1st iteration is the 1st line  stored in text so and so foruth
 
-        istringstream iss(Text); //istringstream is an object from sstream lib and iss is a method that breaks down Text into words,
+        istringstream iss(Text); //istringstream is a class from sstream lib and iss is the object/instance of the class istringstream. Were calling the paramtrized constructor of the class istringstream via the object parameter, which  breaks down Text into words,
         string WordFromLine; //Create a variable of type string which will be used to store each word from the current line of the current iteration
 
-        while(iss >> WordFromLine){ //Niw that 
-            Words.push_back(WordFromLine);
+        while(iss >> WordFromLine){ //This inner while loop will break down the lines into words, we  will traverse through the words. Each word is a iteration, 1st word = 1st iteration etc
+            Words.push_back(WordFromLine); //this is the def of the inner while loop. For every word/iteration, we will add it to our vector 
         }
     }
 
@@ -131,7 +156,7 @@ void Admin::AddStudentRecord(){
 
     cout << "Enter the number of subjects: ";
     cin >> NumSubjects;
-    vector<int>ListOfGrades(NumSubjects); //the number of grade(s) == number of of subjects
+    vector<float>ListOfGrades(NumSubjects); //the number of grade(s) == number of of subjects
     cout <<"    Enter Student ID: ";
     cin >> StudentID;
     cout <<"    Enter Student Name: ";
@@ -142,12 +167,11 @@ void Admin::AddStudentRecord(){
     else{
         cout <<"    Enter " <<  NumSubjects  <<  " grades (comma-separated): "; 
     }
-    for(int i = 0; i<NumSubjects; i++){
+    for(int i = 0; i<NumSubjects; i++){ //user enters grade for each subject
         cin >> ListOfGrades[i];
     }
 
     ofstream FileObject("StudentData.txt", ios::app); //we created an instance of the ofstream class to write to a file named StudentData, the ios:app makes it to where we append rather than overwrite
-
     FileObject << StudentID << "\n";
     FileObject << StudentName << "\n";
     for(int j = 0; j<ListOfGrades.size(); j++){
@@ -158,16 +182,254 @@ void Admin::AddStudentRecord(){
         FileObject << ListOfGrades[j] << ", ";
     }
 
+    FileObject << "\n";
+
     FileObject.close(); //close the file for good practice
 
     cout << "Student Added Successfully \n";
 
-    
-
 }
 
+float Admin::ClassAverageGPA(){
+
+    float Avg = 0;
+    float NumberFromLine = 0;
+    string Text;
+    vector<float>GradesFromFile;
+
+    ifstream File("StudentData.txt");
+
+    while(getline(File, Text)){
+
+        //first remove the the commas between the grades if any for each student
+        for(char &c: Text){
+            if(c == ','){
+                c = ' ';
+            }
+        }
 
 
+        //now, what are we going to do per line for the file?
+        istringstream iss(Text);//It should be noted that the data type of the value passed in the paramtere for the pbject of the class to the constructor must be of a string type 
+        while(iss >> NumberFromLine){
+            GradesFromFile.push_back(NumberFromLine);
+        }
+    }
+
+    for(int i = 0; i<GradesFromFile.size(); i++){
+        Avg = Avg + GradesFromFile[i];
+    }
+
+    Avg = Avg/GradesFromFile.size();
+
+    File.close();
+
+    return Avg;
+}
+
+void Admin::HighestGPA(){
+    float Highest = 0;
+    float NumberFromLine = 0;
+    float PlaceHolder = 0;
+    float Counter = 0;
+    float GPA = 0;
+    string Text;
+    bool Proceed = 0;
+    vector<float>GradesFromFile;
+
+    ifstream File("StudentData.txt");
+
+    while(getline(File, Text)){
+
+        //first remove the the commas between the grades if any for each student
+        for(char &c: Text){
+            if(c == ','){
+                c = ' ';
+            }
+        }
+
+
+        //now, what are we going to do per line for the file?
+        istringstream iss(Text);
+        while(iss >> NumberFromLine){
+            PlaceHolder = PlaceHolder + NumberFromLine;
+            Counter++;
+            Proceed = 1;
+        }
+        if(Proceed){
+            GPA =  PlaceHolder/Counter;
+            GradesFromFile.push_back(GPA);
+            PlaceHolder = 0;
+            Counter = 0;
+            GPA = 0;
+            Proceed = 0;
+        }
+    }
+
+    Highest = GradesFromFile[0];
+    for(int i = 0; i<GradesFromFile.size(); i++){
+        //cout << GradesFromFile[i] << "\n";
+        if(Highest<GradesFromFile[i]){
+            Highest == GradesFromFile[i];
+        }
+    }
+
+    File.close();
+
+    cout << "The highest class average is: " << Highest << "\n";
+}
+
+void Admin::LowestGPA(){
+    float Lowest = 0;
+    float NumberFromLine = 0;
+    float PlaceHolder = 0;
+    float Counter = 0;
+    float GPA = 0;
+    string Text;
+    bool Proceed = 0;
+    vector<float>GradesFromFile;
+
+    ifstream File("StudentData.txt");
+
+    while(getline(File, Text)){
+
+        //first remove the the commas between the grades if any for each student
+        for(char &c: Text){
+            if(c == ','){
+                c = ' ';
+            }
+        }
+
+
+        //now, what are we going to do per line for the file? 
+        istringstream iss(Text);
+        while(iss >> NumberFromLine){
+            PlaceHolder = PlaceHolder + NumberFromLine;
+            Counter++;
+            Proceed = 1;
+        }
+        if(Proceed){
+            GPA =  PlaceHolder/Counter;
+            GradesFromFile.push_back(GPA);
+            PlaceHolder = 0;
+            Counter = 0;
+            GPA = 0;
+            Proceed = 0;
+        }
+    }
+
+    Lowest = GradesFromFile[0];
+    for(int i = 0; i<GradesFromFile.size(); i++){
+        if(Lowest>GradesFromFile[i]){
+            Lowest == GradesFromFile[i];
+        }
+    }
+
+    File.close();
+
+    cout << "The lowest GPA in the class is " << Lowest << "\n";
+
+    
+}
+
+void Admin::LoadStudentRecord(){
+    string Text;
+
+    ifstream File("StudentData.txt");
+
+    while(getline(File, Text)){
+         cout << Text << "\n";
+    }
+
+    File.close();
+}
+
+void Admin::GradeDistribution(){
+    string Text;
+    float NumberfromLine = 0;
+    float PlaceHolder = 0;
+    float Counter = 0;
+    float GPA = 0;
+    bool Proceed = 0;
+    vector<float>GradesFromFile;
+    int GD[9];
+    for(int j = 0; j<9; j++){
+        GD[j] = 0; //set the value to each index (respective grade) to zero by default manually. 
+    }
+
+    ifstream File("StudentData.txt");
+
+    while(getline(File, Text)){
+
+        //first remove the the commas between the grades if any for each student
+        for(char &c: Text){
+            if(c == ','){
+                c = ' ';
+            }
+        }
+
+        istringstream iss(Text);
+        while(iss >> NumberfromLine){
+            PlaceHolder = PlaceHolder + NumberfromLine;
+            Counter++;
+            Proceed = 1;
+        }
+        if(Proceed == 1){
+            //cout << "Placeholder value: " << PlaceHolder << " ";
+            //cout << "Counter value: " << Counter << "\n";
+            GPA = PlaceHolder/Counter;
+            GradesFromFile.push_back(GPA); //remember, a student's grade is based on their GPA.
+            PlaceHolder = 0;
+            Proceed = 0;
+            Counter = 0;
+        }
+
+    }
+
+    for(int i = 0; i<GradesFromFile.size(); i++){
+
+        if((GradesFromFile[i]>= 95) && (GradesFromFile[i]<=100)){ //DONT USE || (OR) logical operator, remeber with or as long as 1/2 conditions are true the whole conditional statment is considered as true. Because almost every student may have a GPA of less than 100, using OR with this specific condition will cause it to be true when someone who has a B grade should get B not A. In short, for ranges, we use && 
+            GD[0]++; //A
+        }
+        else if((GradesFromFile[i]>= 90) && (GradesFromFile[i]<95)){
+            GD[1]++; //A-
+        }
+        else if((GradesFromFile[i]>= 85) && (GradesFromFile[i]<90)){
+            GD[2]++; //B+
+        }
+        else if((GradesFromFile[i]>= 80) && (GradesFromFile[i]<85)){
+            GD[3]++; //B
+        }
+        else if((GradesFromFile[i]>= 75) && (GradesFromFile[i]<80)){
+            GD[4]++; //B-
+        }
+        else if((GradesFromFile[i]>= 70) && (GradesFromFile[i]<75)){
+            GD[5]++; //C+
+        }
+        else if((GradesFromFile[i]>= 65) && (GradesFromFile[i]<70)){
+            GD[6]++; //C
+        }
+        else if((GradesFromFile[i]>= 60) && (GradesFromFile[i]<65)){
+            GD[7]++; //C-
+        }
+        else{
+            GD[8]++; //F
+        }
+
+        cout << GradesFromFile[i] << "\n";
+        
+    }
+
+   cout << "The number of people that got a A: " << GD[0] << "\n";
+   cout << "The number of people that got a A-: " << GD[1] << "\n";
+   cout << "The number of people that got a B+: " << GD[2] << "\n";
+   cout << "The number of people that got a B: " << GD[3] << "\n";
+   cout << "The number of people that got a B-: " << GD[4] << "\n";
+   cout << "The number of people that got a C+: " << GD[5] << "\n";
+   cout << "The number of people that got a C: " << GD[6] << "\n";
+   cout << "The number of people that got a C-: " << GD[7] << "\n";
+   cout << "The number of people that got a F: " << GD[8] << "\n";    
+}
 
 /*
 - If you attempt to read from a file that doesnt exsist there wont be an error but rather the program wont be able to print anything onto the output as there is nothing to read
@@ -179,5 +441,23 @@ void Admin::AddStudentRecord(){
 ofstream $YOUR_OBJECT_NAME_FOR_CLASS_ofstream ("$FILENAME.TXT", ios::app);
 
 (also applies if the file doesnt exsist, it will create it)
+
+- ifstream is a class from the fstream library to read from the text file. Creating an object/instance of the class will have you call the paramtrized constructor which is to open and read from a file
+ifstream $YOUR_OBJECT_NAME_FOR_CLASS_ifstream("$FILENAME.TXT");
+
+- istringstream is a a class from the library sstream. We can use the class to break down every word when we read the current line, from the current iteration of the while loop. from a file via the object of the class
+istringstram  $YOUR_OBJECT_NAME_FOR_CLASS_istringstream("$VARIABLE_OF_TYPE_STRING")
+
+while($YOUR_OBJECT_NAME_FOR_CLASS_istringstream << $VARAIBELE_OF_NUMERICTYPE){ //this while loop will end once there is a word that is read or there is no remainig words within a line 
+    def..
+}
+
+- local variables  are only viewable and accessible within the scope they are declared and intialized.
+Once we re-enter the scope of the creation of the local variable, not only will it be re-created per lines
+but it will also forget the previous value it once stored
+
+- Within the methods that we define for our classes, when declaring the variables of numerical data types (int & float), we must intialize them to 0 expliclity 
+otherwise we risk having garbage values that may potitentally risk in messing up the algorithim in determining the average. 
+
 
 */
